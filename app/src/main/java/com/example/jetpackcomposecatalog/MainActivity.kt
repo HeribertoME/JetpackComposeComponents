244efc429aa148f3e881c8b2bc3d1da22f742c6f
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2024-06-10 22:08:53 -0600

    Se agrega navigation

--- app/src/main/java/com/example/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalog/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.example.jetpackcomposecatalog.ui.components.MyTopBarWithDrawable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.jetpackcomposecatalog.ui.components.ScreenFive
+import com.example.jetpackcomposecatalog.ui.components.ScreenFour
+import com.example.jetpackcomposecatalog.ui.components.ScreenOne
+import com.example.jetpackcomposecatalog.ui.components.ScreenThree
+import com.example.jetpackcomposecatalog.ui.components.ScreenTwo
+import com.example.jetpackcomposecatalog.ui.model.Routes
 import com.example.jetpackcomposecatalog.ui.theme.JetpackComposeCatalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,10 +29,46 @@ class MainActivity : ComponentActivity() {
             JetpackComposeCatalogTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyTopBarWithDrawable()
+                    val navigationController = rememberNavController()
+                    NavHost(
+                        navController = navigationController,
+                        startDestination = Routes.Screen1.route
+                    ) {
+                        composable(Routes.Screen1.route) { ScreenOne(navigationController) }
+                        composable(Routes.Screen2.route) { ScreenTwo(navigationController) }
+                        composable(Routes.Screen3.route) { ScreenThree(navigationController) }
+
+                        // Navigation con un String
+                        /*composable(Routes.Screen4.route + "/{name}") { backStackEntry ->
+                            val name: String? = backStackEntry.arguments?.getString("name")
+                            ScreenFour(navigationController, name.orEmpty())
+                        }*/
+
+                        // Navigation con un Int
+                        composable(
+                            Routes.Screen4.route,
+                            arguments = listOf(navArgument("age") { type = NavType.IntType })
+                        ) { backStackEntry ->
+                            val name: Int? = backStackEntry.arguments?.getInt("age")
+                            ScreenFour(navigationController, name ?: 0)
+                        }
+                        composable(Routes.Screen5.route,
+                            arguments = listOf(navArgument("name") { defaultValue = "Pepe" })
+                        ) { backStackEntry ->
+                            val name: String? = backStackEntry.arguments?.getString("name")
+                            ScreenFive(navigationController, name)
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+                    //MyTopBarWithDrawable()
                     //ScaffoldExample()
                     //SuperHeroStickyView()
                     //SuperHeroWithSpecialControlView()
@@ -64,8 +108,5 @@ class MainActivity : ComponentActivity() {
                             onConfirm = { Log.d("DIALOG", "Hello onConfirm") }
                         )
                     }*/
-                }
-            }
-        }
-    }
+                     */
 }
